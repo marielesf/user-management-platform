74252c9ae7c7d38be27d061d5992ce14b97b182f
marielesf <mariele.fonttana@gmail.com>
2025-01-15 01:22:39 +0000

    Started project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+// import LoginPage from './LoginPage'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        {/* //<LoginPage /> */}
+        LOGIN
+      </div>
+      
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
